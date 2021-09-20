990ee110b56dc8c06c8d3839b10f30d992d95e5b
anhvt <anhvt@ttc-solutions.com>
2021-09-20 11:43:09 +0700

    qr code

--- src/screens/Profile/QRCodeScreen/index.js
+++ src/screens/Profile/QRCodeScreen/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import styles from "./style";
+import {CameraScreen} from 'react-native-camera-kit';
 
 const QRCodeScreen = ({ navigation, route }) => {
   let isSuccess = false;
@@ -10,40 +11,28 @@ const QRCodeScreen = ({ navigation, route }) => {
     isSuccess = true;
     console.log(e);
     if (route.params.onQR) {
-      route.params.onQR(e.data);
+      route.params.onQR(e);
     }
     navigation.goBack();
   };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      {/* <Barcode style={{flex: 1, }} */}
-        {/* onBarCodeRead={onSuccess}/> */}
-      {/* <BarcodeScanner /> */}
-      {/* <BarcodeScanner
-        onBarCodeRead={onSuccess}
-        style={{ flex: 1 }}
-        torchMode={'off'}
-        cameraType={'back'}
-      /> */}
-      {/* <QRCodeScanner
-        cameraStyle={{ height: "100%", width: "100%", alignSelf: "center", justifyContent: "center" }}
-        reactivate={true}
-        showMarker={true}
-        onRead={onSuccess}
-        bottomContent={
-          <View style={styles.bottomView}>
-            <View style={styles.noteText}>
-              <Text style={styles.buttonText}>{String.qrNote}</Text>
-            </View>
-            <View style={styles.bottomContent}>
-              <TouchableOpacity style={styles.buttonTouchable} onPress={()=> navigation.goBack()}>
-                <Text style={styles.buttonText}>{String.cancel}</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+      <CameraScreen
+        showFrame={true}
+        // Show/hide scan frame
+        scanBarcode={true}
+        // Can restrict for the QR Code only
+        laserColor={'blue'}
+        // Color can be of your choice
+        frameColor={'yellow'}
+        // If frame is visible then frame color
+        colorForScannerFrame={'black'}
+        // Scanner Frame color
+        onReadCode={(event) =>
+          onSuccess(event.nativeEvent.codeStringValue)
         }
-      /> */}
+      />
     </SafeAreaView>
   );
 };
