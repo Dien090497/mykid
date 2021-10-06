c7da078df7170883c3b0c937b0caddf1514b6d4b
duybui2704 <buixuanduy2704@gmail.com>
2021-10-06 12:02:32 +0700

    sw-286-ngon-ngu-va-mui-gio-update

--- src/screens/Profile/LanguageTimeZone/styles.js
+++ src/screens/Profile/LanguageTimeZone/styles.js
@@ -51,6 +51,7 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    width: Consts.windowWidth
   },
   scrollView: {
     marginBottom: height * 0.02,
