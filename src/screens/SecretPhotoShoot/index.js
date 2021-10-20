5923805c65c799bf271c1da4b022429ca2322147
Nguyễn Văn Điền <diennv@ttc-solutions.com.vn>
2021-10-20 15:51:31 +0700

    save image

--- src/screens/SecretPhotoShoot/index.js
+++ src/screens/SecretPhotoShoot/index.js
@@ -1,4 +1,4 @@
-import { FlatList, Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Platform, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
 
 import Header from "../../components/Header";
@@ -12,6 +12,8 @@ import { DoSecretShoot } from "../../network/SecretPhotoShootService";
 import DataLocal from "../../data/dataLocal";
 import ActionSheet from "@alessiocancian/react-native-actionsheet";
 import ModalConfirm from "../../components/ModalConfirm";
+import CameraRoll from "@react-native-community/cameraroll";
+import RNFetchBlob from "react-native-fetch-blob";
 
 export default ({ navigation }) => {
   const refLoading = useRef();
@@ -19,124 +21,125 @@ export default ({ navigation }) => {
   const refConfirm = useRef();
   const [isSetting, setIsSetting] = useState(false);
   let sheet = null;
+  const [selectItem, setSelectItem] = useState(null);
   const [imageMook, setImageMook] = useState([
     {
       id: 1,
-      src: 'https://www.codeproject.com/KB/GDI-plus/ImageProcessing2/img.jpg?w=144',
+      src: "https://www.codeproject.com/KB/GDI-plus/ImageProcessing2/img.jpg?w=144",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 2,
-      src: 'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      src: "https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 3,
-      src: 'https://images.prismic.io/mystique/5d7c09b9-40e5-4254-ae1c-2c1cb59aa898_IMG3.jpg?auto=compress,format',
+      src: "https://images.prismic.io/mystique/5d7c09b9-40e5-4254-ae1c-2c1cb59aa898_IMG3.jpg?auto=compress,format",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 4,
-      src: 'http://www.imgworlds.com/wp-content/uploads/2016/04/img-worlds-of-adventure-new-low.jpg',
+      src: "http://www.imgworlds.com/wp-content/uploads/2016/04/img-worlds-of-adventure-new-low.jpg",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 5,
-      src: 'https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_720/w_80,x_15,y_15,g_south_west,l_klook_water/activities/ype8x0zkqbv239asgx9p/C%C3%B4ngVi%C3%AAnGi%E1%BA%A3iTr%C3%ADIMGWorldsofAdventure.jpg',
+      src: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_720/w_80,x_15,y_15,g_south_west,l_klook_water/activities/ype8x0zkqbv239asgx9p/C%C3%B4ngVi%C3%AAnGi%E1%BA%A3iTr%C3%ADIMGWorldsofAdventure.jpg",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 6,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTkfqSS3HXpmwsCw9o7Va2X5AOKe1fZ1RHyQ&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTkfqSS3HXpmwsCw9o7Va2X5AOKe1fZ1RHyQ&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 7,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYI8AEKvGxJDd_v5MePWAjOC-FGTnl2C8ETw&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYI8AEKvGxJDd_v5MePWAjOC-FGTnl2C8ETw&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 8,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8K8xAqYJPCg-thTg7wye3Im1-pXE0R3mkPw&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8K8xAqYJPCg-thTg7wye3Im1-pXE0R3mkPw&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 9,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwx4wK3n7yfMbrUt4OwOg4NkzpM_B9STQLag&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwx4wK3n7yfMbrUt4OwOg4NkzpM_B9STQLag&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 10,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeGxXAc_WUPBI9t7XX5Fq9Up3wNL1hZew4hA&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeGxXAc_WUPBI9t7XX5Fq9Up3wNL1hZew4hA&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 11,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3b44SiI8YNwuRwWiu313vZa8UIjjaEaI_cA&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3b44SiI8YNwuRwWiu313vZa8UIjjaEaI_cA&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 12,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ4muDMMy55GhJJ3VlnjI5-dTwzeJxgDg2uQ&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ4muDMMy55GhJJ3VlnjI5-dTwzeJxgDg2uQ&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 13,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZc9Jx1K9dGjSl5YtTGVtkR8MaIFNtCYhJlw&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZc9Jx1K9dGjSl5YtTGVtkR8MaIFNtCYhJlw&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 14,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaOozW9XX9wkRs4nAe0K9DMzIO_0qq54_0WQ&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaOozW9XX9wkRs4nAe0K9DMzIO_0qq54_0WQ&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 15,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBNh1ud6Y-mvQSpQf1_eKurPyr8AQinTIc9Q&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBNh1ud6Y-mvQSpQf1_eKurPyr8AQinTIc9Q&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 16,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbJqAw1eXY_Bs2K3Ze5NDxQeRA6znQTlZykA&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbJqAw1eXY_Bs2K3Ze5NDxQeRA6znQTlZykA&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 17,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREDA75AfMu-DtlqZCR34T59ipm3R7azGxHUg&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREDA75AfMu-DtlqZCR34T59ipm3R7azGxHUg&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 18,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7KpUvb7pED3OyE6MakTBxX1W1rI_rd2yqoQ&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7KpUvb7pED3OyE6MakTBxX1W1rI_rd2yqoQ&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 19,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4IUo_CTu-wIhjUPXxwzPqq1rRY-lONhqTUw&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4IUo_CTu-wIhjUPXxwzPqq1rRY-lONhqTUw&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
     {
       id: 20,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwSA6W7w2EY2Z817RMFqVhrJtom2sdlccMgg&usqp=CAU',
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwSA6W7w2EY2Z817RMFqVhrJtom2sdlccMgg&usqp=CAU",
       isChoose: false,
       date: new Date(),
     },
@@ -149,7 +152,7 @@ export default ({ navigation }) => {
       data[item.index].isChoose = !data[item.index].isChoose;
       setImageMook(data);
     } else {
-      refImage.current.openModal(imageMook,item.index)
+      refImage.current.openModal(imageMook, item.index);
     }
   };
 
@@ -159,42 +162,76 @@ export default ({ navigation }) => {
 
   const shootImage = () => {
     DoSecretShoot(
-      DataLocal.deviceId,{
-        success: data =>{
-          console.log(data)
+      DataLocal.deviceId, {
+        success: data => {
+          console.log(data);
         },
-        refLoading
-      }
-    )
+        refLoading,
+      },
+    );
   };
 
-  const action = () =>{
-    return(
-      <ActionSheet options={[String.delete,'Lưu ảnh',String.cancel]}
+  const action = () => {
+    return (
+      <ActionSheet options={[String.delete, "Lưu ảnh", String.cancel]}
                    onPress={handleImageAction}
                    cancelButtonIndex={2}
                    ref={o => sheet = o}
       />
-    )
-  }
+    );
+  };
 
   const handleImageAction = async (index) => {
-    switch (index){
+    switch (index) {
       case 0:
-        refConfirm.current.open('Ban co muon xoa?', ()=>{console.log('Xoa')})
+        refConfirm.current.open("Ban co muon xoa?", () => {
+          console.log("Xoa");
+        });
         break;
       case 1:
-        console.log('345');
+        saveImage(selectItem.src);
         break;
     }
-  }
+  };
+
+  const saveImage = (src) => {
+    if (Platform.OS === "ios") {
+      CameraRoll.save(src)
+        .then(console.log("Photo added to camera roll!"))
+        .catch(err => console.log("err:", err));
+    } else {
+      RNFetchBlob
+        .config({
+          fileCache: true,
+          appendExt: "jpg",
+        })
+        .fetch("GET", src)
+        .then((res) => {
+          console.log();
+          CameraRoll.saveToCameraRoll(res.path())
+            .then((res) => {
+              console.log("save", res);
+            }).catch((error) => {
+            console.log("error", error);
+          });
+
+        });
+    }
+  };
 
   const renderItem = (item) => {
     return (
       <View style={styles.itemList}>
-        <TouchableOpacity style={styles.imageList} onPress={()=>{touchImage(item)}} onLongPress={()=>{sheet.show(item)}}>
+        <TouchableOpacity style={styles.imageList}
+                          onPress={() => {
+                            touchImage(item);
+                          }}
+                          onLongPress={() => {
+                            sheet.show();
+                            setSelectItem(item.item);
+                          }}>
           <FastImage
-            source={{uri:item.item.src}}
+            source={{ uri: item.item.src }}
             resizeMode={FastImage.resizeMode.stretch}
             style={styles.imageItem}
           />
@@ -223,7 +260,7 @@ export default ({ navigation }) => {
       />
       <View style={styles.mainView}>
         <FlatList
-          style={{ width: "100%", flex:1 }}
+          style={{ width: "100%", flex: 1 }}
           data={imageMook}
           renderItem={renderItem}
           keyExtractor={item => item.id}
@@ -241,7 +278,7 @@ export default ({ navigation }) => {
         </View>
       </View>
       {action()}
-      <PreViewImage ref={refImage}/>
+      <PreViewImage ref={refImage} />
       <ModalConfirm ref={refConfirm} />
       <LoadingIndicator ref={refLoading} />
     </View>
