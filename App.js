875743ec6fbbf6b188006c82a0c13d833102754d
TNT <thanhtn@ttc-solutions.com.vn>
2021-11-18 09:51:23 +0700

    tnt push config firebase

--- App.js
+++ App.js
@@ -1,17 +1,56 @@
+import {StatusBar} from "react-native";
+import React, { useEffect} from 'react';
 import {AlertDropHelper} from './src/functions/AlertDropHelper';
 import {Colors} from './src/assets/colors/Colors';
 import DropdownAlert from 'react-native-dropdownalert';
 import {Provider} from 'react-redux';
-import React from 'react';
 import Routes from './src/routes';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {StatusBar} from 'react-native';
 import redux from './src/redux/config/redux';
 import './src/constants/IMLocalize';
+import {fcmService} from './src/FCMService'
+import {localNotificationService} from './src/LocalNotificationService'
 
-class App extends React.Component {
-  render() {
-    return (
+export default function App() {
+
+  useEffect(() => {
+    fcmService.registerAppWithFCM()
+    fcmService.register(onRegister, onNotification, onOpenNotification)
+    localNotificationService.configure(onOpenNotification)
+
+    function onRegister(token) {
+      console.log("[App] onRegister: ", token)
+    }
+
+    function onNotification(notify) {
+      console.log("[App] onNotification: ", notify)
+      const options = {
+        soundName: 'default',
+        playSound: true,
+      }
+      localNotificationService.showNotification(
+        0,
+        notify.title,
+        notify.body,
+        notify,
+        options
+      )
+    }
+
+    function onOpenNotification(notify) {
+      console.log("[App] onOpenNotification: ", notify)
+      alert("Open Notification: " + notify.body)
+    }
+
+    return () => {
+      console.log("[App] unRegister")
+      fcmService.unRegister()
+      localNotificationService.unregister()
+    }
+
+  }, [])
+
+  return (
       <SafeAreaProvider>
         <Provider store={redux.store}>
           <Routes />
@@ -30,7 +69,5 @@ class App extends React.Component {
         </Provider>
       </SafeAreaProvider>
     );
-  }
 }
 
-export default App;
