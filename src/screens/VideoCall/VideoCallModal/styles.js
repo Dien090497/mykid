6fc0f9176205b7c0199a75b8fccdc0ff1a2f1a3e
anhvt <anhvt@ttc-solutions.com>
2021-11-24 09:58:19 +0700

    - update ui video call

--- src/screens/VideoCall/VideoCallModal/styles.js
+++ src/screens/VideoCall/VideoCallModal/styles.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   },
   containerFooter: {
     height: '10%',
-    backgroundColor: '#494954',
+    backgroundColor: Colors.blackTransparent,
     justifyContent: 'center',
     alignItems: 'center',
   },
