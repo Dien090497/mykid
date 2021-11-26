25d75eaf8efb9b93e81120b0881befd15bec743e
duybui2704 <buixuanduy2704@gmail.com>
2021-11-26 08:54:35 +0700

    fix_bug_19/11

--- src/constants/translations/vi/common.js
+++ src/constants/translations/vi/common.js
@@ -321,7 +321,7 @@ export default {
   'txtForgotPassWord1': '1.Xin vui lòng nhập số điện thoại hợp lệ để nhận mã xác minh.',
   'txtForgotPassWord2': '2. Nhập xác nhận thiết lập lại mật khẩu mã xác minh là  thành công.',
   'inputTxt': 'Vui lòng nhập số điện thoại của bạn',
-  'txtContinue ': 'Tiếp tục',
+  'txtContinue': 'Tiếp tục',
   'getCode': 'Lấy mã',
   'successPassword': 'Cập nhật mật khẩu thành công',
   'newPassword': 'Nhập mật khẩu mới',
