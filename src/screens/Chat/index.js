f879fe8581451eed90ae207d35e4cb21dec56ec3
Diennv <diennv@ttx-solutions.com.vn>
2021-12-30 11:23:20 +0700

    fix bug chat

--- src/screens/Chat/index.js
+++ src/screens/Chat/index.js
@@ -78,6 +78,7 @@ export default function Chat({navigation}) {
           <RefreshControl
             onRefresh={()=>{
               setDevices(XmppClient.lstRoom)
+              XmppClient.updateRooms()
             }}
             refreshing={false} />
         }
