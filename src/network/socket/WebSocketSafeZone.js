3a4b5cd7aa9fc3d3c38a1a57c8e8d72dddd30782
duybx <1234560ac>
2022-01-06 10:23:42 +0700

    fix pattern

--- src/network/socket/WebSocketSafeZone.js
+++ src/network/socket/WebSocketSafeZone.js
@@ -15,12 +15,9 @@ export default class WebSocketSafeZone {
   static isConnected = false;
   static navigationRef = null;
 
-  // static PATTERN = [
-  //   0, 500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170,
-  //   40, 500,
-  // ];
   static PATTERN = [
-    0, 100
+    0, 500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170,
+    40, 500,
   ];
 
   static setReconnect(autoReconnect) {
