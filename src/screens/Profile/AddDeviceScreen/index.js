387f5f0261d24dc36e043ae486faad7991fa8159
anhvt <anhvt@ttc-solutions.com>
2022-01-06 15:05:46 +0700

    - update screen

--- src/screens/Profile/AddDeviceScreen/index.js
+++ src/screens/Profile/AddDeviceScreen/index.js
@@ -147,7 +147,7 @@ const AddDeviceScreen = ({ navigation, route }) => {
   }
 
   const onNavigateHome = () => {
-    navigation.replace(Consts.ScreenIds.HomeMainScreen);
+    navigation.replace(Consts.ScreenIds.Tabs);
     setModal(false);
     setShowModal(false);
     reduxStore.store.dispatch(commonInfoAction.reset());
