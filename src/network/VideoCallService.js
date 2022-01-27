3829d160d90f38a217cf3e547c5e714ccf310165
Diennv <diennv@ttx-solutions.com.vn>
2022-01-27 10:57:13 +0700

    fix bug video call

--- src/network/VideoCallService.js
+++ src/network/VideoCallService.js
@@ -1,4 +1,4 @@
-import {post} from './http/HttpClient';
+import {post, get} from './http/HttpClient';
 
 import {createVideoCallUrl} from './http/ApiUrl';
 
@@ -24,4 +24,12 @@ export function rejectVideoCallApi(
 ) {
   const url = [createVideoCallUrl, id, 'reject'].join('/');
   return post(url, {body, success, failure, autoShowMsg, refLoading, refNotification});
+}
+
+export function getInfoVideoCallApi(
+  id,
+  {success, failure, autoShowMsg = true, refLoading = null, refNotification = null} = {},
+) {
+  const url = [createVideoCallUrl, id, 'status'].join('/');
+  return get(url, { success, failure, autoShowMsg, refLoading, refNotification});
 }
